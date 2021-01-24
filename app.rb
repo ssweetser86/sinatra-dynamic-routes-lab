7a0c6ce5c4d556e1e15e95a6de633a2baaf6353f
Stephen <stephenjsweets@gmail.com>
2021-01-24 13:19:37 -0500

    commit tree 1b54cb5fc3cda93c35774ed5f786999b3db29f64

--- app.rb
+++ app.rb
@@ -1,6 +1,39 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].reverse!
+    "#{@name}"
+  end
+
+  get '/square/:number' do
+    @square = params[:number].to_i ** 2
+    "#{@square}"
+  end
+
+  get '/say/:number/:phrase' do
+    @num = params[:number].to_i
+    @arr = Array.new(@num, params[:phrase])
+    "#{@arr.join(' ')}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    case params[:operation]
+    when "add"
+      "#{params[:number1].to_i + params[:number2].to_i}"
+    when "subtract"
+      "#{params[:number1].to_i - params[:number2].to_i}"
+    when "multiply"
+      "#{params[:number1].to_i * params[:number2].to_i}"
+    when "divide"
+      "#{params[:number1].to_i / params[:number2].to_i}"
+    else
+      "Unknown operation"
+    end
+  end
 
 end
